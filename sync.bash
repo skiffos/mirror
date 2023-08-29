60dc573507d3657d67d3d2041688365d410f3086
Christian Stewart <christian@aperture.us>
2023-08-29 14:59:17 -0700

    sync.bash: add sync script
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ sync.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+
+aws \
+    --profile skiff-mirror \
+    s3 \
+    --endpoint-url https://s3.us-east-005.backblazeb2.com \
+    sync ./ s3://skiffos-mirror/ \
+    --delete \
+    --exclude ".git" \
+    --exclude "sync.bash"
